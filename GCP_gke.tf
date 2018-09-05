e6de3538c0cf5365696b7b0cb844dd634a37b7b8
Brad Downey <brad@magic7s.com>
2018-09-05 11:19:57 -0300

    Remove outputs of certs. Create local provisioner to download kubeconfig via gcloud api

--- GCP_gke.tf
+++ GCP_gke.tf
@@ -27,17 +27,14 @@ resource "google_container_cluster" "primary" {
 
     tags = ["foo", "bar"]
   }
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials ${ var.GKE_name }"
+    environment = { KUBECONFIG = "./kubeconfig_${var.GKE_name}"}
+  }
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "rm -f ./kubeconfig_${var.GKE_name}"
+  }
 }
 
-# The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
-}
 
-output "client_key" {
-  value = "${google_container_cluster.primary.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
-}
