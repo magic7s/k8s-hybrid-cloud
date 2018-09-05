8e8140feb82aef33422ae51836205ed106db7c61
Brad Downey <brad@magic7s.com>
2018-09-05 11:13:55 -0300

    change var name to match name scheme

--- AWS_common.tf
+++ AWS_common.tf
@@ -1,6 +1,7 @@
 // Common configurations for AWS
 variable AWS_region {}
-variable AWS_worker_groups {type = "list"}
+variable EKS_worker_groups {type = "list"}
+variable EKS_name {}
 variable AWS_vpc_name {}
 variable AWS_vpc_subnet {}
 variable AWS_azs { type = "list" }
