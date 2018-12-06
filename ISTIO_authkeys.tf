17d135e413c839cdbd80b9004a0d6be88ed65819
Brad Downey <brad@magic7s.com>
2018-12-06 11:53:27 -0800

    update auth keys trigger

--- ISTIO_authkeys.tf
+++ ISTIO_authkeys.tf
@@ -1,7 +1,10 @@
 // After Istio is installed. Install service accounts.
 
 resource "null_resource" "istio-istio-remote-script" {
-
+  triggers = {
+    cluster_instance_cert_primary = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
+    cluster_instance_cert_remote  = "${module.eks.cluster_certificate_authority_data}"
+  }
   provisioner "local-exec" {
     command = "./put_istio_remote_auth.sh ${ local.remote_cluster_kubeconfig } ${var.EKS_name} ${ local.control_cluster_kubeconfig }"
   }
