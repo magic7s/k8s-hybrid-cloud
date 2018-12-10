e47a0289c3de8dc7935ff105ed9074beaded0041
Brad Downey <brad@magic7s.com>
2018-12-09 17:58:37 -0800

    remove auth file on destroy

--- ISTIO_authkeys.tf
+++ ISTIO_authkeys.tf
@@ -8,7 +8,10 @@ resource "null_resource" "istio-istio-remote-script" {
   provisioner "local-exec" {
     command = "./put_istio_remote_auth.sh ${ local.remote_cluster_kubeconfig } ${var.EKS_name} ${ local.control_cluster_kubeconfig }"
   }
-  
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "rm ./remote-cluster-auth"
+  }  
   depends_on = ["helm_release.istio-remote-eks", "helm_release.istio-control-gke"]
 }
 
